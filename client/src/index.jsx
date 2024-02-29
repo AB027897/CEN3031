2b5bfc817b064afadd637ea2f19b8fa9634c3fcf
Jodi Foster <33043348+JohhnSmithh@users.noreply.github.com>
2024-02-29 15:10:24 -0500

    attempted to send data from .py to .jsx and output to website
    Tutorial I followed: https://www.youtube.com/watch?v=7LNl2JlZKHA

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './index.css';
 import Home from './App';
 
 export default function App() {
--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
-@app.route('/')
-def home():
-    return "Hello World"
+#CORS(app)
+@app.route("/test")
+def members():
+    return {"members": ["Member1", "Member2", "Member3"]}
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=3000, debug=True)
+    app.run(debug=True)
+    #app.run(host="127.0.0.1", port=3000, debug=True)
