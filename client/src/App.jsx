172b4f5a357a6938ae5c2d1f12daee52a82e6eae
Jodi Foster <33043348+JohhnSmithh@users.noreply.github.com>
2024-03-01 14:18:51 -0500

    Log-In UI
    I replaced App.jsx and App.css since I didn't know how to display a new jsx folder (requires flask?)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link" 
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="Title">DonorGram</h1>
       </header>
+      <body className="App-body">
+        <div className="TextEntry">
+            <h2>Username</h2>
+            <input className="TextField" type="text" defaultValue="Enter Text..."/>
+        </div>
+        <div className="TextEntry">
+            <h2>Password</h2>
+            <input className="TextField" type="text" defaultValue="Enter Text..."/>
+        </div>
+        <p className="ErrorText">* No account found. Incorrect username or password</p>
+        <p className="button">LOGIN</p>
+        <p className="RedirectText">Need an account? <a className="App-link" href="http://localhost:3000/sign-up" rel="noopener noreferrer">SIGN UP</a></p>
+      </body>
     </div>
   );
 }
---
+++ client/src/LogIn.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function LogIn() {
+    return (
+        <div>
+        
+        </div>
+    );
+}
+export default LogIn();
