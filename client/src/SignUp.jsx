0991ba078c548c260a01536dd086c8877cc7bddc
Jake Nut <amekness123@gmail.com>
2024-03-06 01:10:56 -0500

    Added Signup Screen

---
+++ client/src/SignUp.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState } from 'react';
+import './SignUp.css'
+
+
+function SignUp() {
+    const [getDonorStatus, setDonorStatus] = useState(false);
+    const [getCharityStatus, setCharityStatus] = useState(false);
+    const changeState = (type) => {
+        if(type === "donor") {
+            setDonorStatus(true);
+            setCharityStatus(false);
+        } else {
+            setCharityStatus(true);
+            setDonorStatus(false);
+        }
+    }
+    return (
+        <div className = "Page">
+            <p className="Title">DonorGram</p>
+            <p className="Account-Type">Account Type</p>
+            <div className="Account-Type-Div">
+                <input type = "radio" value="Donor" checked={getDonorStatus} onClick={()=> changeState("donor")}/>
+                <p className="Type">Donor</p> 
+                <input type = "radio" value="Charity" checked={getCharityStatus} onClick={()=> changeState("charity")}/>
+                <p className="Type">Charity</p>
+            </div>
+            <p className="Username">Username</p>
+            <input className="Text-Field" type="text" placeholder='Enter Text'/>
+        </div>
+        
+    );
+}
+
+
+export default SignUp;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import Home from './App';
+import SignUp from './SignUp'
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>}>
-      </Route>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
     </Routes>
     </BrowserRouter>
   );
