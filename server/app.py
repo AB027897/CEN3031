f96f5412d8906794c6f7e19e6c80735bf20922f3
khh7 <kim.monica@ufl.edu>
2024-03-12 21:37:39 -0400

    Login route and functionality

--- server/app.py
+++ server/app.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, request, send_file, Response
 from flask_cors import CORS
+import json
+import os
+# from firebase import *
+from firebase import init_app, create_user, authenticate_user
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../client/build")
 CORS(app)
-@app.route('/')
-def home():
-    return "Hello World"
+init_app()
+
+
+@app.route('/signupvalidation', methods=['POST'])
+def validateSignup():
+    account = json.loads(request.headers["account"])
+    email = account['email']
+    password = account['password']
+    return Response(create_user(email, password), status=200, mimetype="text")
+    
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    if not email or not password:
+        return Response("Email and password are required.", status=400, mimetype="text/plain")
+    result = authenticate_user(email, password)
+    if isinstance(result, str):
+        return Response(result, status=401, mimetype="text/plain")
+    else:
+        return Response("Login successful.", status=200, mimetype="text/plain")
+
+
+
+@app.route('/', defaults={'file': ''})
+@app.route('/<path:file>')
+def serve_file(file):
+    path = app.static_folder + "/" + file
+    if file != "" and os.path.exists(path):
+        return send_file(path)
+    else:
+        return send_file(os.path.join(app.static_folder, 'index.html'))
+    
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=3000, debug=True)
---
+++ server/firebase.py
@@ -0,0 +1,40 @@
+import pyrebase
+import os
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+
+cred = {
+    "apiKey": os.getenv("API_KEY"),
+    "authDomain": os.getenv("AUTH_DOMAIN"),
+    "projectId": os.getenv("PROJECT_ID"),
+    "storageBucket": os.getenv("STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"), 
+    "appId": os.getenv("APP_ID"),
+    "measurementId": os.getenv("MEASUREMENT_ID"),
+    "databaseURL" : ""
+}
+
+
+def init_app():
+    firebase_app= pyrebase.initialize_app(cred)
+    global auth 
+    auth = firebase_app.auth()
+
+
+def create_user(email, password):
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+    except Exception as err:
+        error = json.loads(err.args[1])
+        return error["error"]["message"]
+
+
+def authenticate_user(email, password):
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        return user
+    except Exception as err:
+        error = json.loads(err.args[1])
+        return error["error"]["message"]
