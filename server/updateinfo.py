676c6a61a0c2b2eb2e6140fe041b71a6e61f3a11
samkim711 <samuelkim0821@gmail.com>
2024-04-03 16:01:21 -0400

    update email

--- server/updateinfo.py
+++ server/updateinfo.py
@@ -1,58 +1,11 @@
-import pyrebase
-import os
-from dotenv import load_dotenv
-import json
-
-load_dotenv()
-
-cred = {
-    "apiKey": os.getenv("API_KEY"),
-    "authDomain": os.getenv("AUTH_DOMAIN"),
-    "projectId": os.getenv("PROJECT_ID"),
-    "storageBucket": os.getenv("STORAGE_BUCKET"),
-    "serviceAccount" : "./serviceAccount.json",
-    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
-    "appId": os.getenv("APP_ID"),
-    "measurementId": os.getenv("MEASUREMENT_ID"),
-    "databaseURL" : os.getenv("DATABASE_URL")
-}
-
-firebase = pyrebase.initialize_app(cred)
-db = firebase.database()
-storage = firebase.storage()
-auth = firebase.auth()
+import firebase_admin
+from firebase_admin import credentials, auth
 
 
-def update_email(user_id, email):
-    ref = "accounts/{}".format(user_id)
-    cur = db.child(ref).get().val()
-    cur["email"] = email
-    db.child(ref).update(cur)
-
-
-def update_account_type(user_id, account_type):
-    ref = "accounts/{}".format(user_id)
-    cur = db.child(ref).get().val()
-    cur["account type"] = account_type
-    db.child(ref).update(cur)
-
-
-def update_name(user_id, name):
-    ref = "accounts/{}".format(user_id)
-    cur = db.child(ref).get().val()
-    cur["name"] = name
-    db.child(ref).update(cur)
-
-
-def update_phone_number(user_id, phone):
-    ref = "accounts/{}".format(user_id)
-    cur = db.child(ref).get().val()
-    cur["phone number"] = phone
-    db.child(ref).update(cur)
-
-
-def update_type(user_id, charity_type):
-    ref = "accounts/{}".format(user_id)
-    cur = db.child(ref).get().val()
-    cur["type"] = charity_type
-    db.child(ref).update(cur)
+def update_email(user_id, new_email):
+    try:
+        user = auth.update_user(user_id, email = new_email)
+        ref = db.child("accounts").child(user_id)
+        ref.update({"email": new_email})
+    except Exception as e:
+        print('Error updating user:', e)
