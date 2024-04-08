1c70627a8c29ef3288800aa667a9ed9e654fbbb5
Jodi Foster <33043348+JohhnSmithh@users.noreply.github.com>
2024-04-08 16:45:39 -0400

    mostly reworked home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,14 @@ function App() {
   return (
     <div className={s.App}>
       <header className={s.LogoDiv}>
-        <img src={logo_e} className={s.Earth} alt="logo" />
+        <div className={s.Earth}>
+          <img src={logo_e} className={s.EarthImg} alt="logo" />
+        </div>
         <img src={logo_h} className={s.Hands} alt="hands" />
         <p className={s.Title}> DonorGram</p>
       </header>
-      <div className={s.Links}>
-        <p className={s.JoinNowText}>Join Today</p>
+      <div className={s.LinksDiv}>
+      <p className={s.JoinNowText}>Join Today</p>
         <a href="/signup" rel="noopener noreferrer"><button className={s.Button1}>Create Account</button></a>
         <p className={s.AlreadyAccountText}>Already have an account?</p>
         <a href="/login" rel="noopener noreferrer"><button className={s.Button2}>Sign In</button></a>
