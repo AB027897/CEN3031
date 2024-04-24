97802ed43d485fc21195f1b597da951ea5214905
samkim711 <samuelkim0821@gmail.com>
2024-04-24 18:03:46 -0400

    comments

--- server/firebase.py
+++ server/firebase.py
@@ -5,6 +5,7 @@ import json
 
 load_dotenv()
 
+# credentials based on the .env file
 cred = {
     "apiKey": os.getenv("API_KEY"),
     "authDomain": os.getenv("AUTH_DOMAIN"),
@@ -17,17 +18,18 @@ cred = {
     "databaseURL" : os.getenv("DATABASE_URL")
 }
 
+# initialize the variables and the applications
 def init_app():
     global firebase_app
     firebase_app = pyrebase.initialize_app(cred)
     global auth 
     auth = firebase_app.auth()
 
-    
-
+# return the firebase application
 def get_firebase():
     return firebase_app
 
+# create a user with email and password
 def create_user(email, password):
     try:
         user = auth.create_user_with_email_and_password(email, password)
@@ -36,7 +38,7 @@ def create_user(email, password):
         error = json.loads(err.args[1])
         return str(error["error"]["message"])
 
-
+# authenticate the user with email and password
 def authenticate_user(email, password):
     try:
         user = auth.sign_in_with_email_and_password(email, password)
@@ -45,6 +47,7 @@ def authenticate_user(email, password):
         error = json.loads(err.args[1])
         return str(error["error"]["message"])
 
+# create a token for the user
 def create_token(user):
     try:
         token = auth.create_custom_token(user['localId'])
@@ -52,7 +55,8 @@ def create_token(user):
     except Exception as err:
         error = json.loads(err.args[1])
         return str(error["error"]["message"])
-
+    
+# signing in only using some token
 def signin_token(token):
     try:
         user = auth.sign_in_with_custom_token(token)
